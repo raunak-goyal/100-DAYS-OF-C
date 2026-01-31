64c95b890cad319073d50415b12a6b211ee57964
Raunak Goyal <ugccrpo@gmail.com>
2026-01-31 23:22:59 +0530

    Added Day26

---
+++ Day26d.c
@@ -0,0 +1,68 @@
+// Q51: Write a program to print the following pattern:
+//     5
+//    45
+//   345
+//  2345
+// 12345
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+    5
+   45
+  345
+ 2345
+12345
+
+*/
+
+//     #include <stdio.h>
+// int main() {
+//     int n = 5;
+
+//     for (int i = 5; i >= 1; i--) {         // controls starting number
+//         for (int s = 1; s < i; s++) {      // prints spaces
+//             printf(" ");
+//         }
+//         for (int j = i; j <= n; j++) {     // prints numbers from i to 5
+//             printf("%d", j);
+//         }
+//         printf("\n");
+//     }
+
+// //     return 0;
+// // }
+// Q52: Write a program to print the following pattern:
+
+// *
+
+// *
+// *
+// *
+
+// *
+// *
+// *
+// *
+// *
+
+// *
+// *
+// *
+
+// *
+
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
+
+
