c7893c80424559e15091589e8c240772eb7a5a62
Raunak Goyal <ugccrpo@gmail.com>
2026-02-01 12:37:19 +0530

    added day 27

---
+++ Day27d.c
@@ -0,0 +1,38 @@
+// Q53: Write a program to print the following pattern:
+// *
+// ***
+// *****
+// *******
+// *********
+// *******
+// *****
+// ***
+// *
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+*/
+
+#include<stdio.h>
+int main(){
+
+
+
+
+    
+    return 0;
+
+}
