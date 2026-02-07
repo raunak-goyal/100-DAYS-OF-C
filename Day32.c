e629bdeb7ca6f17703c9f96bec7aeeaf8d4f428c
Raunak Goyal <ugccrpo@gmail.com>
2026-02-07 20:36:37 +0530

    added Day 32

---
+++ Day32.c
@@ -0,0 +1,93 @@
+//Q63: Merge two arrays.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+1 2 3
+2
+4 5
+Output 1:
+1 2 3 4 5
+
+*/
+
+
+// #include<stdio.h>
+// int main(){
+// int n;
+// int x;
+// printf("enter the number of elements in array n :");
+// scanf("%d",&n);
+// printf("enter the number of elements in array x :");
+// scanf("%d",&x);
+// int arr1[n];
+// int arr2[x];
+// printf("Enter array 1 = ");
+// for(int i=0;i<n;i++){
+//     printf("enter elements of array 1 : ");
+//     scanf("%d",&arr1[i]);
+    
+
+// }
+
+// printf("\n Enter array 2 = ");
+// for(int i=0;i<x;i++){
+//     printf("enter elements of array 2: ");
+//     scanf("%d",&arr2[i]);
+// }
+
+
+// int z[n+x];
+// for(int i =0;i<n;i++){
+//     z[i]=arr1[i];
+
+// }
+// for(int i =0;i<x;i++){
+//     z[n+i]=arr2[i];
+// }
+
+// printf("merged array:");
+// for(int i=0;i<n+x;i++){
+// printf(" %d ",z[i]);
+// }
+//     return 0;
+// }
+
+//Q64: Find the digit that occurs the most times in an integer number.
+
+/*
+Sample Test Cases:
+Input 1:
+112233
+Output 1:
+1
+
+Input 2:
+887799
+Output 2:
+7
+
+*/
+#include<stdio.h>
+int main(){
+    int n ;
+    printf("enter value of n: ");
+scanf("%d",&n);
+int x[];
+for(int i=0;i<n;i++){
+printf("enter value of x: ");
+scanf("%d",&x[i]);
+int x=10;
+
+
+
+
+
+
+
+
+
+return 0;
+}
+}
