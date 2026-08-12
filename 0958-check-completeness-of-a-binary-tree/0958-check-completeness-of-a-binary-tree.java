20ac8319643854b2ea03660bf8951a6a2ab3ba00
Raunak Goyal <ugccrpo@gmail.com>
2026-08-12 23:58:25 +0530

    Time: 1 ms (82.85%), Space: 43.8 MB (60.32%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+
+    public boolean isCompleteTree(TreeNode root) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        boolean foundNull = false;
+
+        while (!queue.isEmpty()) {
+
+            TreeNode current = queue.poll();
+
+            if (current == null) {
+                foundNull = true;
+            } 
+            else {
+
+                // A node appears after a missing position
+                if (foundNull)
+                    return false;
+
+                queue.offer(current.left);
+                queue.offer(current.right);
+            }
+        }
+
+        return true;
+    }
+}
